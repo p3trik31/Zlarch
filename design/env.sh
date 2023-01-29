db0add9f9249cf30f29756ffe0c15abf0acc3c73
p3trik31 <patrik.ch3@seznam.cz>
2023-01-29 14:44:17 +0000

    upload

--- design/env.sh
+++ design/env.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-mkdir /usr/share/zlarch
 
+
+
+  #naklonovat git
+
+mkdir -p /usr/share/zlarch
+
+
+  #zkopirovat git do slozky
 
 grub_on() {
-  if command -v grub-install > /dev/null; then
-    # GRUB is installed, perform desired action
-    echo "GRUB is installed and running"
-    echo 'GRUB_COLOR="light-orange/black"' >> /etc/default/grub
+  if command -v grub-install > /dev/null; then    
+    echo 'GRUB_COLOR_NORMAL="white/black"' >> /etc/default/grub
+    echo 'GRUB_COLOR_HIGHLIGHT="black/light-gray"' >> /etc/default/grub
+    echo 'GRUB_BACKGROUND="/usr/share/backgrounds/xfce/xfce-evil.jpg"' >> /etc/default/grub
+
+    sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT=".*"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
+
+
   else
     # GRUB is not installed
-    echo "GRUB is not installed"
+    echo "GRUB neni stazen"
   fi
 }
+
+
+grub_on
+
+
+exit 0
