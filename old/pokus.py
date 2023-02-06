e74646d6b64d3e90a7be4e568462ad194c07b43f
p3trik31 <patrik.ch3@seznam.cz>
2023-02-06 14:19:27 +0000

    dotfiles

--- old/pokus.py
+++
@@ -1,8 +0,0 @@
-import json
-import logging
-import os
-import time
-#from profiles import xfce4
-import archinstall
-
-#archinstall.run_custom_user_commands(archinstall.run_custom_user_commands(['mkdir /tmp'], he , showLog = False))
