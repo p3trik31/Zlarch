942f0e1b938ef973d25d57af95b9184a8d4fa3b8
p3trik31 <patrik.ch3@seznam.cz>
2023-02-26 18:05:10 +0000

    repo

--- design/env.sh
+++ design/env.sh
@@ -2,15 +2,15 @@
 
 echo '[zlarch-repo]' >> /etc/pacman.conf
 echo 'SigLevel = Optional' >> /etc/pacman.conf
-echo 'Server = http://35.207.153.25/zlarch-repo/' >> /etc/pacman.conf
+echo 'Server = http://35.207.153.25/zlarch-repo/' >> /etc/pacman.conf  #vlastni repozitar
 
 
 cp -r /Zlarch/design/zlarch/ /usr/share/
 cp -r /Zlarch/design/.config /etc/skel
 
-pacman -Sy 
+pacman -Sy mugshot #stazeni xfce4 balicku z vlastniho repozitare, protoze neni v oficialnim
 
-grub_on() {
+grub_on() {     #uprava vzhledu pokud je nainstalovan grub
   if command -v grub-install > /dev/null; then    
     echo 'GRUB_COLOR_NORMAL="black/black"' >> /etc/default/grub
     echo 'GRUB_COLOR_HIGHLIGHT="white/dark-gray"' >> /etc/default/grub
@@ -25,7 +25,7 @@ grub_on() {
 }
 
 
-lightdm() {
+lightdm() {     #uprava vzhledu lightdm
   echo 'background=/usr/share/zlarch/wallpaper.svg' >> /etc/lightdm/lightdm-gtk-greeter.conf
   echo 'theme-name = Adapta' >> /etc/lightdm/lightdm-gtk-greeter.conf
   echo 'indicators = ~host;~spacer;~clock;~spacer;~layout;~language;~session;~a11y;~power' >> /etc/lightdm/lightdm-gtk-greeter.conf
@@ -39,7 +39,7 @@ lightdm
 
 users=$(ls /home)
 
-for user in $users; do
+for user in $users; do    #aplikace vzhledu pro existujici uzivatale
   cp -r /etc/skel/.config /home/$user
   chown -R $user:$user /home/$user/.config
 done
